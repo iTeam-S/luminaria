da4fbe7c6e652f64607e22dfafdf2f24f6fc61f3
naziradidier <nazirap20.aps1b@gmail.com>
2023-05-26 19:46:11 +0300

    [add] tete et pied de la page

---
+++ front-luminaria/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+      <footer>
+        <p>This is the footer of my React app.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
