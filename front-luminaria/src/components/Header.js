0a6d99e5a5d03b8a4758c498e9246e7a813858ad
naziradidier <nazirap20.aps1b@gmail.com>
2023-06-07 10:55:18 +0300

    [modification] nav

--- front-luminaria/src/components/Header.js
+++ front-luminaria/src/components/Header.js
@@ -5,12 +5,13 @@ function Header() {
     <header>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+          <a class="navbar-brand" href="#">
+            <img src="./logoluminaria.jpeg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+            luminaria
+          </a>
+          </div>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
@@ -32,11 +33,10 @@ function Header() {
               </li>
             </ul>
             <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            
               <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
           </div>
-        </div>
       </nav>
     </header>
   );
