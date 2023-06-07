ab2a199226da5580b47dd9bc8888a360f970704d
3li07 <mahery.bemanantsoa@esti.mg>
2023-06-08 02:02:00 +0300

    Mise en place du maquette

--- front-luminaria/src/components/Header.js
+++ front-luminaria/src/components/Header.js
@@ -3,38 +3,43 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="./logoluminaria.jpeg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="./logoluminaria.jpeg" alt="Logo" width="30" height="30" className="d-inline-block align-text-top"/>
             luminaria
           </a>
           </div>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  ABOUT US
                 </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <ul className="dropdown-menu modif-drop">
+                  <li><a className="modif-drop-item" href="#">HISTORY</a></li>
+                  <li><a className="modif-drop-item" href="#">TEAM</a></li>
+                  <li><a className="modif-drop-item" href="#">FINANCIAL</a></li>
                 </ul>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  OUR WORK
+                </a>
+                <ul className="dropdown-menu modif-drop">
+                  <li><a className="modif-drop-item" href="#">APPROCH</a></li>
+                  <li><a className="modif-drop-item" href="#">PUBLICATION</a></li>
+                  <li><a className="modif-drop-item" href="#">BLOG</a></li>
+                  <li><a className="modif-drop-item" href="#">DOCUMENTS</a></li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">CONTACT</a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
+            <form className="d-flex" role="search">
             
-              <button class="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-outline-dark mx-3 shadow-merch" type="submit">MERCH</button>
             </form>
           </div>
       </nav>
