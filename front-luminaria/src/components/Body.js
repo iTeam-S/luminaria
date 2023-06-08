27a6e780f2a82c903e3aeb37c190ba0ae5d4a5c2
3li07 <mahery.bemanantsoa@esti.mg>
2023-06-08 10:33:21 +0300

    info association

--- front-luminaria/src/components/Body.js
+++ front-luminaria/src/components/Body.js
@@ -27,7 +27,7 @@ function Body () {
                 </svg>
             </div>
             <div style={{display: "flex", justifyContent: "center"}}>
-            <button data-aos="zoom-in" className="btn btn-outline-dark mx-3 shadow-merch" type="submit">More  <i class="fas fa-light fa-arrow-down fa-lg mx-1"></i></button>
+            <button data-aos="zoom-in" className="btn btn-outline-dark mx-3 shadow-merch" type="submit">More  <i className="fas fa-light fa-arrow-down fa-lg mx-1"></i></button>
             </div>
 
             <div data-aos="flip-up" className="cardTete">
@@ -35,7 +35,7 @@ function Body () {
                 <div className="row">     
                     <div data-aos="fade-up" className="col-md-4 start-col">
                         <div className="content-descr">
-                            <h2><span><i class="fa-solid fa-spinner fa-spin"></i></span> EN COURS</h2>
+                            <h2><span><i className="fa-solid fa-spinner fa-spin"></i></span> EN COURS</h2>
                             <p>Lore
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -46,7 +46,7 @@ function Body () {
                     </div>
                     <div data-aos="fade-up" className="col-md-4 mid-col">
                         <div className="content-descr">
-                            <h2><span><i class="fa-solid fa-square-check fa-bounce"></i></span> ACHEVEE</h2>
+                            <h2><span><i className="fa-solid fa-square-check fa-bounce"></i></span> ACHEVEE</h2>
                             <p>Lore
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -57,7 +57,7 @@ function Body () {
                     </div>
                     <div data-aos="fade-up" className="col-md-4 end-col">
                         <div className="content-descr">
-                            <h2><span><i class="fa-solid fa-play fa-fade"></i></span> A VENIR</h2>
+                            <h2><span><i className="fa-solid fa-play fa-fade"></i></span> A VENIR</h2>
                             <p>Lore
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -66,7 +66,101 @@ function Body () {
                             </p>
                         </div>
                     </div>
-                </div>      
+                </div>
+            </div>
+
+            <div className="info">
+                <div className= "row style-row" >
+                    <div data-aos="fade-right" className="col-md-6">
+                        <div className="content-Item">
+                            <h1>Why</h1>
+                            <h2>Luminaria ?</h2>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                    <div data-aos="fade-left" className="col-md-6">
+                        <div className="content-Image">
+                            <svg className="svgLeft" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                            <div className="content-Image-carre"></div>
+                            <svg className="svgRight" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div className= "row style-row" >
+                    <div data-aos="fade-right" className="col-md-6">
+                        <div className="content-Item">
+                            <h1>OUR</h1>
+                            <h2>Volunteer</h2>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                    <div data-aos="fade-left" className="col-md-6">
+                        <div className="content-Image">
+                            <svg className="svgLeft" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                            <div className="content-Image-carre"></div>
+                            <svg className="svgRight" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div className= "row style-row" >
+                    <div data-aos="fade-right" className="col-md-6">
+                        <div className="content-Item">
+                            <h1>OUR</h1>
+                            <h2>Partners</h2>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                    <div data-aos="fade-left" className="col-md-6">
+                        <div className="content-Image">
+                            <svg className="svgLeft" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                            <div className="content-Image-field"></div>
+                            <div className="content-Image-field" style={{backgroundColor: "#F6A713"}}></div>
+                            <div className="content-Image-field"></div>
+                            <svg className="svgRight" xmlns="http://www.w3.org/2000/svg" >
+                                <line x1="0" y1="0" x2="150" y2="0" stroke="black" strokeWidth="2" /> 
+                                <line x1="0" y1="0" x2="0" y2="150" stroke="black" strokeWidth="2" />             
+                                <path d="M0,100 A100,100 0 0,1 100,0" fill="none" stroke="black" strokeWidth="1" /> 
+                            </svg>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
